64401ee2987d2de048999b8c2d113e5d7b35fb04
Rohit <sahrohitkumar342@gmail.com>
2025-07-03 18:29:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const card = document.getElementById('card');
+
+card.addEventListener('click', function () {
+  card.classList.toggle('is-flipped');
+});
+
+function showMore() {
+  alert("🎊 Surprise! You're amazing day and so loved. Enjoy your day Happy Birthday Ansita!");
+}
